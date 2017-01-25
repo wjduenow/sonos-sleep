e600b6f941f11c3df337bd46e6f34b8a4970f976
Wes Duenow <wjduenow@raybeam.com>
2017-01-25 09:24:25 -0800

    More WIP

--- run.py
+++ run.py
@@ -1,17 +1,12 @@
 from eve import Eve
 import soco
-#from soco import SoCo
 
 app = Eve()
 
-@app.route('/test')
-def test():
-    return 'Test'
+@app.route('/sleep')
+def sleep():
 
-@app.route('/hello')
-def hello_world():
-
-#  try:
+  try:
     zones = soco.discover()
     for zone in zones:
         if zone.player_name == 'Master Bedroom':
@@ -20,19 +15,33 @@ def hello_world():
     playlists = sonos.get_music_library_information('sonos_playlists')
     for playlist in playlists:
         print playlist.title
-        if playlist.title == 'The Gambler': #'Sleep':
+        if playlist.title == 'Sleep':
             new_pl = playlist
 
     sonos.clear_queue()
     sonos.add_to_queue(new_pl)
     sonos.play()
 
-    return 'hello world!'
+    return 'Running Sleep Routine'
 
-#  except: 
-#    return 'error'
+   except Exception as e:
+     return ("error: %s" % (e))
 
+@app.route('/wake')
+def wake():
 
+  try:
+    zones = soco.discover()
+    for zone in zones:
+        if zone.player_name == 'Master Bedroom':
+            sonos = zone
+
+    sonos.pause()
+
+    return 'Running Wake Routine'
+
+   except Exception as e:
+     return ("error: %s" % (e))
 
 
 if __name__ == '__main__':
